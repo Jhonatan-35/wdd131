d91abd6a3c8c0d451b51286ed55c275192fc1204
Jhonatan-35 <jhonatanyugsanpomatoca@gmail.com>
2024-09-21 03:15:17 -0500

    Special something

--- getdates.js
+++
@@ -1,3 +0,0 @@
-document.getElementById("copyright-year").innerHTML = new Date().getFullYear();
-let oLastModif = new Date(document.lastModified);
-let text = document.lastModified;document.getElementById("demo").innerHTML = text;
